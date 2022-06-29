678f0b899147a6c1ddff5dfbe423b3e22f0d0edf
Kong, Paul <Xiangbao.Kong@fmr.com>
2022-06-29 22:27:00 +0800

    Add support for PORT and PROXY env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const { ApolloServer } = require('apollo-server');
+const { bootstrap: bootstrapGlobalAgent } = require('global-agent');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
+
+// Setup global support for environment variable based proxy configuration.
+bootstrapGlobalAgent();
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
@@ -14,7 +18,7 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url, port } = await server.listen();
+  const { url, port } = await server.listen({port: process.env.PORT || 4000});
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
